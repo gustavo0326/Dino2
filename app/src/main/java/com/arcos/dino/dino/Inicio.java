06471fb1c5838fb5b94d414217b7349391969cbf
admin <gustavo0326@gmail.com>
2017-03-07 22:24:04 -0500

    subir codigo

--- app/src/main/java/com/arcos/dino/dino/Inicio.java
+++ app/src/main/java/com/arcos/dino/dino/Inicio.java
@@ -6,15 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Inicio extends AppCompatActivity {
 
-
+    private GifImageView gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        gif=(GifImageView)findViewById(R.id.gifanime);
+
+        try {
+            InputStream inputStream=getAssets().open("dino_mov_cab.gif");
+            byte[] bytes= IOUtils.toByteArray(inputStream);
+            gif.setBytes(bytes);
+            gif.startAnimation();
+
+        }catch (IOException e){
+
+        }
+
+
     }
+
+
 
 
     public void cambiar(View view) {
