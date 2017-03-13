a1fb536196caa45d077eba1c9e87abdbf82b5fd9
admin <gustavo0326@gmail.com>
2017-03-13 13:20:36 -0500

    subir codigo

--- app/src/main/java/com/arcos/dino/dino/LoginActivity.java
+++ app/src/main/java/com/arcos/dino/dino/LoginActivity.java
@@ -1,23 +1,94 @@
 package com.arcos.dino.dino;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.arcos.dino.dino.view.AllGameActivity;
+import com.arcos.dino.dino.view.ContainerActivity;
 import com.arcos.dino.dino.view.CrearCuenta;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+private GoogleApiClient googleApiClient;
+    private SignInButton loginGoogleBtn;
+    public static final int codigoLogin=777;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //objeto de opciones para obtener los datos
+        GoogleSignInOptions options=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        //autenticacion
+        googleApiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,options)
+                .build();
+        loginGoogleBtn =(SignInButton)findViewById(R.id.googleBtn);
+        loginGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,codigoLogin);
+            }
+        });
     }
 
     public void crearCuenta(View view){
         Intent intent=new Intent(LoginActivity.this, CrearCuenta.class);
         startActivity(intent);
 
+    }
+
+    public void bottom(View view) {
+        Intent intent=new Intent(LoginActivity.this, ContainerActivity.class);
+        startActivity(intent);
+    }
+//metodo que valiidad si la conexion falla
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    //metodo llegan los resultados
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==codigoLogin){
+            //selector o inicio de session
+           GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //despues de obtener el resultado se lo enviamos a otro metodo
+            datosLogin(result);
+        }
+    }
+
+    private void datosLogin(GoogleSignInResult result) {
+        //verifica si la operacion es exitosa
+        if (result.isSuccess()){
+            //abrimos la activity donde mostrar los datos
+            showData();
+
+        }else {
+            Toast.makeText(this, "No fue posible iniciar session", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showData() {
+        //inicia la actividad con loas banderas
+        Intent intent=new Intent(this,AllGameActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/arcos/dino/dino/view/AllGameActivity.java
@@ -0,0 +1,112 @@
+package com.arcos.dino.dino.view;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.arcos.dino.dino.LoginActivity;
+import com.arcos.dino.dino.R;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class AllGameActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private ImageView photo;
+    private TextView mail,id,name;
+    private GoogleApiClient googleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_game);
+
+        photo= (ImageView) findViewById(R.id.perfil);
+        mail= (TextView) findViewById(R.id.mail);
+        id= (TextView) findViewById(R.id.id);
+        name= (TextView) findViewById(R.id.name);
+
+        GoogleSignInOptions options=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        googleApiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,options)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> ini=Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (ini.isDone()){
+GoogleSignInResult result=ini.get();
+            resultadosLogin(result);
+        }else {
+ini.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+    @Override
+    public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+        resultadosLogin(googleSignInResult);
+    }
+});
+        }
+
+    }
+
+    private void resultadosLogin(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            GoogleSignInAccount cuenta=result.getSignInAccount();
+            name.setText(cuenta.getDisplayName());
+            mail.setText(cuenta.getEmail());
+            id.setText(cuenta.getId());
+            Glide.with(this).load(cuenta.getPhotoUrl()).into(photo);
+
+        }else {
+            sinDatos();
+
+        }
+    }
+
+    private void sinDatos() {
+        Intent i=new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    public void salir(View view) {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()){
+                    sinDatos();
+                }else {
+                    Toast.makeText(getApplicationContext(), "no se pudo cerrar sesion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void revocar(View view) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
